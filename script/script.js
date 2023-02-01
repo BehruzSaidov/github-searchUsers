074b08d271ba7c8932689f0b2ab18d9f272489a3
Behruz Saidov <behruzsaidov3374@gmail.com>
2023-02-01 11:36:09 +0500

    add css animate

--- script/script.js
+++ script/script.js
@@ -18,6 +18,7 @@ if( localStorage.length > 0 && localStorage.getItem("dark") == "true" ){
 search_btn.addEventListener('click',function(){
 
     if(!/ /gi.test(search_input.value) && search_input.value.length > 0){
+        search_input.value = "";
        render();
        getUser(url+search_input.value).then(render).catch(remove)
     }   
